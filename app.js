4e00bc3556b5a0ec320477646971a820af8b59f3
Vinícius Silva <vinicius.ls@live.com>
2021-08-23 00:53:08 -0300

    feat: added app handle impl

--- app.js
+++ app.js
@@ -1,4 +1,79 @@
-exports.handler = (event, context, callback) => {
-  console.log('Hello, logs!');
-  callback(null, 'great success');
-}
+// dependencies
+const AWS = require('aws-sdk');
+const util = require('util');
+const sharp = require('sharp');
+
+// get reference to S3 client
+const s3 = new AWS.S3();
+
+exports.handler = async (event, context, callback) => {
+  // Read options from the event parameter.
+  console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
+  const srcBucket = event.Records[0].s3.bucket.name;
+
+  // Object key may have spaces or unicode non-ASCII characters.
+  const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+  const dstBucket = `${srcBucket}/thumbnails`;
+  const dstKey = `${srcKey}`;
+
+  // Infer the image type from the file suffix.
+  const typeMatch = srcKey.match(/\.([^.]*)$/);
+  if (!typeMatch) {
+    console.log("Could not determine the image type.");
+    return;
+  }
+
+  // Check that the image type is supported
+  const imageType = typeMatch[1].toLowerCase();
+  if (imageType != "jpg" && imageType != "png") {
+    console.log(`Unsupported image type: ${imageType}`);
+    return;
+  }
+
+  // Download the image from the S3 source bucket.
+
+  let origImage;
+  try {
+    const params = {
+      Bucket: srcBucket,
+      Key: srcKey
+    };
+    origImage = await s3.getObject(params).promise();
+
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  // set thumbnail width. Resize will set the height automatically to maintain aspect ratio.
+  const width  = 200;
+
+  // Use the sharp module to resize the image and save in a buffer.
+  let buffer;
+  try {
+    buffer = await sharp(origImage.Body).resize(width).toBuffer();
+
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  // Upload the thumbnail image to the destination bucket
+  try {
+    const destParams = {
+      Bucket: dstBucket,
+      Key: dstKey,
+      Body: buffer,
+      ContentType: "image"
+    };
+
+    const putResult = await s3.putObject(destParams).promise();
+    console.log(putResult);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log('Successfully resized ' + srcBucket + '/' + srcKey +
+    ' and uploaded to ' + dstBucket + '/' + dstKey);
+};
