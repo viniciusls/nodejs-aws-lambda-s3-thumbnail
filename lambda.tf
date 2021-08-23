5469db49a251fc7e30bd1b00f93017e354143950
Vinícius Silva <vinicius.ls@live.com>
2021-08-23 01:44:01 -0300

    feat: impl terraform script

--- lambda.tf
+++ lambda.tf
@@ -8,17 +8,30 @@ variable "aws_region" {
 }
 
 provider "aws" {
-  region          = var.aws_region
+  region = var.aws_region
 }
 
 data "archive_file" "lambda_zip" {
-  type          = "zip"
-  source_file   = "index.js"
-  output_path   = "aws_lambda_s3_thumbnail.zip"
+  type = "zip"
+  source_dir = path.module
+  output_path = "aws_lambda_s3_thumbnail.zip"
+  excludes = [
+    "aws_lambda_s3_thumbnail.zip",
+    ".git",
+    ".gitignore",
+    ".idea",
+    ".terraform",
+    ".terraform.lock.hcl",
+    "terraform.tfstate",
+    ".terraform.tfstate.lock.info",
+    "lambda.tf",
+    "LICENSE",
+    "README.md",
+    "yarn.lock"]
 }
 
-resource "aws_iam_role" "iam_for_lambda_tf" {
-  name = "iam_for_lambda_tf"
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
 
   assume_role_policy = <<EOF
 {
@@ -29,23 +42,7 @@ resource "aws_iam_role" "iam_for_lambda_tf" {
       "Principal": {
         "Service": "lambda.amazonaws.com"
       },
-      "Effect": "Allow",
-      "Sid": ""
-    },
-    {
-      "Action": [
-        "s3:Get*",
-        "s3:List*",
-        "s3:PutObject"
-      ]
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Resource": [
-        "arn:aws:s3:::vini-images-example/*",
-      ]
-      "Effect": "Allow",
-      "Sid": ""
+      "Effect": "Allow"
     }
   ]
 }
@@ -53,20 +50,20 @@ EOF
 }
 
 resource "aws_lambda_permission" "allow_bucket" {
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.func.arn
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.vini-images-example.arn
+  statement_id = "AllowExecutionFromS3Bucket"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.aws_lambda_s3_thumbnail.arn
+  principal = "s3.amazonaws.com"
+  source_arn = aws_s3_bucket.vini-images-example.arn
 }
 
 resource "aws_lambda_function" "aws_lambda_s3_thumbnail" {
-  filename         = "aws_lambda_s3_thumbnail.zip"
-  function_name    = "aws_lambda_s3_thumbnail"
-  role             = aws_iam_role.iam_for_lambda_tf.arn
-  handler          = "app.handler"
+  filename = "aws_lambda_s3_thumbnail.zip"
+  function_name = "aws_lambda_s3_thumbnail"
+  role = aws_iam_role.iam_for_lambda.arn
+  handler = "app.handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime          = "nodejs14.x"
+  runtime = "nodejs14.x"
 }
 
 resource "aws_s3_bucket" "vini-images-example" {
@@ -77,24 +74,28 @@ resource "aws_s3_bucket_notification" "bucket_notification_png" {
   bucket = aws_s3_bucket.vini-images-example.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.func.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "images/"
-    filter_suffix       = ".png"
+    lambda_function_arn = aws_lambda_function.aws_lambda_s3_thumbnail.arn
+    events = [
+      "s3:ObjectCreated:*"]
+    filter_prefix = "images/"
+    filter_suffix = ".png"
   }
 
-  depends_on = [aws_lambda_permission.allow_bucket]
+  depends_on = [
+    aws_lambda_permission.allow_bucket]
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification_jpg" {
   bucket = aws_s3_bucket.vini-images-example.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.func.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "images/"
-    filter_suffix       = ".jpg"
+    lambda_function_arn = aws_lambda_function.aws_lambda_s3_thumbnail.arn
+    events = [
+      "s3:ObjectCreated:*"]
+    filter_prefix = "images/"
+    filter_suffix = ".jpg"
   }
 
-  depends_on = [aws_lambda_permission.allow_bucket]
+  depends_on = [
+    aws_lambda_permission.allow_bucket]
 }
